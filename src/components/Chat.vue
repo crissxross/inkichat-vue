9b2c9562390008d1d5307df86c87913ab241cff1
Christine Wilks <crissxross@gmail.com>
2018-01-18 11:09:47 +0000

    sending messages at intervals basic implementation

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="container-grid">
-    <!-- <small style="background-color: grey">(msg. {{ msgId }} ) chosen option: {{ chosenOption }} </small> -->
+    <!-- p for TESTING only -->
+    <p style="color: grey"> <small>Send messages:</small>
+      <button @click="startSendingMessages">START</button>
+      <button @click="stopSendingMessages">STOP</button>
+      <button @click="sendNextMessage">Next one</button>
+      <small>(msg. {{ msgId }} ) chosen option: {{ chosenOption }}</small>
+    </p>
     <app-message
-      v-for="msg in chatdata"
+      v-for="msg in displayedMessages"
       :chatMsg="msg"
       :key="msg.id">
     </app-message>
@@ -21,19 +27,50 @@ export default {
   data() {
     return {
       chatdata: dialogChatData,
+      currentMsgId: 0,
+      currentMessage: [],
+      displayedMessages: [],
+      start: false,
+      sendInterval: null,
+      delay: 1000,
       chosenOption: '',
       msgId: ''
     };
   },
   created() {
-    console.log('Chat component created');
+    console.log('chatdata.length:', this.chatdata.length);
+    this.startSendingMessages();
     eventBus.$on('optionChosen', (option, id) => {
       this.chosenOption = option;
       this.msgId = id;
       console.log('Chat component handles event choice:', this.chosenOption, this.msgId);
     });
+  },
+  methods: {
+    startSendingMessages() {
+      this.sendInterval = setInterval(this.sendNextMessage, this.delay);
+      this.start = true;
+      console.log('start:', this.start);
+    },
+    sendNextMessage() {
+      if (this.currentMsgId < this.chatdata.length) {
+        const nextMsgId = this.currentMsgId + 1;
+        this.currentMessage = this.chatdata.slice(this.currentMsgId, nextMsgId);
+        this.currentMsgId++;
+        console.log('currentMsgId updated:', this.currentMsgId);
+        return this.displayedMessages.push(this.currentMessage[0]);
+      } else {
+        console.log('No more messages to send!');
+        this.stopSendingMessages();
+      }
+    },
+    stopSendingMessages() {
+      clearInterval(this.sendInterval);
+      this.start = false;
+      console.log('start:', this.start);
+    }
   }
-
+  // Does anything need to be a computed property?
 };
 </script>
 
@@ -44,5 +81,11 @@ export default {
   padding: 10px 5px 10px;
   /* padding: 5px; */
   color: #F4F4F4;
+}
+/* TEMPORARY STYLES FOR TESTING PURPOSES */
+button {
+  cursor: pointer;
+  padding: 2px;
+  background-color: gainsboro;
 }
 </style>
